f42ea95965fae19ea31f1f85aa986c425346fd3a
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-10 20:22:13 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,6 +36,7 @@ def count_contestants_by_hometown(data, hometown)
   counter = 0
   data.each do |season_number, contestant_array|
     contestant_array.each do |contestant|
+      
       contestant.each do |stat_keys, stats|
         if contestant["hometown"] == hometown
           counter += 1
@@ -43,7 +44,7 @@ def count_contestants_by_hometown(data, hometown)
           counter
         end
       end
-      binding.pry
+  
       counter
     end
   end
