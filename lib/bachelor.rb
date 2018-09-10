e0ba512e282054ec27db6910d184610132099f6f
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-10 21:12:43 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -65,6 +65,7 @@ def get_average_age_for_season(data, season)
     end
   end
 sum_of_ages = ages.inject(0) { |sum, i| sum + i }
+binding.pry
 average_age = sum_of_ages / ages.length
 average_age
 end
